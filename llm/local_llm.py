c4ebd52257618d4a2918ea880701fbaa80654f0c
acoulibaly <acoulibaly@lcr-ci.com>
2025-09-30 13:29:52 +0000

    switch to groq

--- llm/local_llm.py
+++ llm/local_llm.py
@@ -1,24 +1,28 @@
 """
 Module pour charger et utiliser le modèle Ollama via LangChain (Chat interface)
 """
-
-from langchain_community.chat_models import ChatOllama
+import os
+from langchain_core import callbacks
+from langchain_groq import ChatGroq
+# from langchain_ollama.chat_models import ChatOllama
 # from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
 
+load_dotenv()
 class LocalLLM:
-    def __init__(self, model_name: str = "mistral"):
+    def __init__(self, model_name: str = "meta-llama/llama-4-scout-17b-16e-instruct"):
         """
         Initialise le modèle Ollama local via l'interface Chat
         """
         callback_manager = [StreamingStdOutCallbackHandler()]
-        
-        self.llm = ChatOllama(
-            base_url="http://172.16.10.92:11434",
-            model=model_name,
-            callback_manager=callback_manager,
-            temperature=0.7,
+        api_key = os.getenv("GROQ_API_KEY")
+        self.llm = ChatGroq(
+            api_key=api_key,
+            model=model_name, 
+            callback_manager=callback_manager, 
+            temperature=0.7
         )
 
     def generate_response(self, prompt: str) -> str | None:
@@ -35,6 +39,6 @@ class LocalLLM:
 
 
 # Exemple d'utilisation
-# llm = LocalLLM("mistral")  # ou "llama2", "phi3", etc.
+# llm = LocalLLM()  # ou "llama2", "phi3", etc.
 # response = llm.generate_response("Explique-moi les différences entre TCP et UDP.")
 # print(response)
